64673ec6e8778bb3ca283412033d7dc95746053f
mrincredibletx <164132028+mrincredibletx@users.noreply.github.com>
2026-09-14 10:45:37 +0530

    ISOWBT

--- .venv/Scripts/activate.bat
+++ .venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\rkban\OneDrive\Desktop\111\age\.venv
+set VIRTUAL_ENV=C:\Users\rkban\OneDrive\Desktop\111\Ai Agent\.venv
 
 if not defined PROMPT set PROMPT=$P$G
 
